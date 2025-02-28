ddc21e6a49577363394aeae1c4700c2113f7a25d
Afrina Tithy <afrina.tithy@gmail.com>
2025-02-28 22:23:42 +0600

    capitalizeFirstLetter.js

---
+++ capitalizeFirstLetter.js
@@ -0,0 +1,7 @@
+function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+
+console.log(capitalizeFirstLetter("hello"));  
+console.log(capitalizeFirstLetter("world"));  
